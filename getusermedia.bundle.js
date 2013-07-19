62845f43147bd75d76bfff8c3301e7213b2d3abb
Henrik Joreteg <hjoreteg@gmail.com>
2013-07-18 17:01:31 -0700

    better error handling

--- getusermedia.bundle.js
+++ getusermedia.bundle.js
@@ -7,23 +7,29 @@ var func = (navigator.getUserMedia ||
             navigator.msGetUserMedia);
 
 
-module.exports = function (contstraints, cb) {
+module.exports = function (constraints, cb) {
     var options;
     var haveOpts = arguments.length === 2;
     var defaultOpts = {video: true, audio: true};
 
-    // make contstraints optional
+    // make constraints optional
     if (!haveOpts) {
-        cb = contstraints;
-        contstraints = defaultOpts;
+        cb = constraints;
+        constraints = defaultOpts;
     }
 
     // treat lack of browser support like an error
-    if (!func) return cb(new Error('notSupported'));
+    if (!func) {
+        // throw proper error per spec
+        var error = new Error('NavigatorUserMediaError');
+        error.reason = "NOT_SUPPORTED";
+        return cb(error);
+    }
 
-    func.call(navigator, contstraints, function (stream) {
+    func.call(navigator, constraints, function (stream) {
         cb(null, stream);
     }, function (err) {
+        err.reason = err.name || "PERMISSION_DENIED";
         cb(err);
     });
 };
